d51c400f57969f38b831a342bda6aad73c13330d
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-02-18 18:15:11 +0200

    up

--- SeminarHub/Controllers/SeminarController.cs
+++ SeminarHub/Controllers/SeminarController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
 using SeminarHub.Data;
 using SeminarHub.Data.Models;
 using SeminarHub.Models.ViewModels;
